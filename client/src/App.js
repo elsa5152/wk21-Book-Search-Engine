35fa95e345a3ff665f1e73d44dfeb71f91d7c9bb
Elsa <100567053+elsa5152@users.noreply.github.com>
2022-08-06 19:47:28 +0930

    make new file

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+//import Aopllo server
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost'
+
+//define ApollpLoent
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem("id_token");
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  },
+  uri: "/graphql",
+});
 
 function App() {
   return (
