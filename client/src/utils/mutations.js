26a13536cc9215abd776dafbb030ce8fb40f002f
Elsa <100567053+elsa5152@users.noreply.github.com>
2022-08-09 21:14:06 +0930

    work

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,8 +25,8 @@ export const ADD_USER = gql`
 `;
 
 export const SAVE_BOOK = gql`
-  mutation saveBook($book: BookInput!) {
-    saveBook(book: $book) {
+  mutation saveBook($bookData: BookInput!) {
+    saveBook(bookData: $bookData) {
       username
       email
       bookCount
